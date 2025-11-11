90554eef37b7caddfd56500f6d180f06ce4b116b
Gautam Rana <gautamrana5968@gmail.com>
2025-11-11 12:20:12 +0530

    git initialize , eas initializwd

--- android/app/src/main/java/com/gautamranaji/seneiorcitizenhelpdeskapp/MainActivity.kt
+++ android/app/src/main/java/com/gautamranaji/seneiorcitizenhelpdeskapp/MainActivity.kt
@@ -1,4 +1,4 @@
-package com.anonymous.FieldTrackingApp
+package com.gautamranaji.seneiorcitizenhelpdeskapp
 import expo.modules.splashscreen.SplashScreenManager
 
 import android.os.Build
--- android/app/src/main/java/com/gautamranaji/seneiorcitizenhelpdeskapp/MainApplication.kt
+++ android/app/src/main/java/com/gautamranaji/seneiorcitizenhelpdeskapp/MainApplication.kt
@@ -1,4 +1,4 @@
-package com.anonymous.FieldTrackingApp
+package com.gautamranaji.seneiorcitizenhelpdeskapp
 
 import android.app.Application
 import android.content.res.Configuration
